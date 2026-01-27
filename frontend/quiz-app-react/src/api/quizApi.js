ceede0ff3f6bfe311c3b1e5654e1665f677087c2
Vishwareddy5 <vishwatejareddy696@gmail.com>
2026-01-27 16:08:33 +0530

    Use VITE_API_BASE_URL for backend API

--- frontend/quiz-app-react/src/api/quizApi.js
+++ frontend/quiz-app-react/src/api/quizApi.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_BASE_URL = "https://quiz-backend-86af.onrender.com";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const createQuiz = (category, numQ, title) => {
   return axios.post(`${API_BASE_URL}/quiz/create`, null, {
